c5359a1ecb0f5fb2627a2406be0d2d83a853e1f7
Toru <toru.inoue@kissaki.tv>
2018-03-11 12:32:51 +0900

    add project.

---
+++ Assets/HTTP_v2018.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HTTP_v2018 : MonoBehaviour
+{
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        var req = UnityWebRequest.Post("https://httpbin.org/post", "data");
+        var response = req.SendWebRequest();
+        yield return response;
+
+        if (req.isHttpError)
+        {
+            Debug.LogError("http error:" + req.error + " code:" + req.responseCode + " " + req.downloadHandler.text);
+        }
+
+    }
+
+}
